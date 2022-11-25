388848f080d962907ed97b6a357f822d95b1c181
Guy Niv <gn9118@gmail.com>
2022-11-25 09:41:22 +0200

    wip

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route("/")
-def index():
-    return "Hello World!"
 
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+app.run()
