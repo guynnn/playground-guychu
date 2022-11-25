fa401171d175702aea8316a75940b98b41b48163
Guy Niv <gn9118@gmail.com>
2022-11-25 10:05:19 +0200

    wip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
 from flask import send_file
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,6 +8,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
+    print("## Headers: ", request.headers)
     return send_file("./Resources/dye.mp3",as_attachment=True)
 
 app.run()
