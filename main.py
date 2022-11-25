47c3dced1be30591bfbb96bf021fc348280b72f7
Guy Niv <gn9118@gmail.com>
2022-11-25 10:31:15 +0200

    wip

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import flask
 from flask import send_file
 from flask import request
+from flask import redirect
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+@app.route('/download', methods=['GET'])
 def home():
-    print("## Headers: ", request.headers)
+    print("## Headers in download: ", request.headers)
+    return redirect("127.0.0.1:5000/yay", code=302)
+    # return send_file("./Resources/dye.mp3",as_attachment=True)
+
+@app.route('/yay', methods=['GET'])
+def yay():
+    print("## Headers in yay: ", request.headers)
     return send_file("./Resources/dye.mp3",as_attachment=True)
 
 app.run()
