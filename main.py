737a7ad5a77c5e2a0bb3ca5a5ac7f8b8c430af51
Guy Niv <gn9118@gmail.com>
2022-11-25 12:59:13 +0200

    wip

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ import flask
 from flask import send_file
 from flask import request
 from flask import redirect
+import os
 from waitress import serve
 
 app = flask.Flask(__name__)
 
+hostName = "playground-guychu.herokuapp.com/"
 
 @app.route('/download/', methods=['GET'])
 def download():
     print("## Headers in download: ", request.headers)
-    return redirect("http://127.0.0.1:1245/yay/", code=302)
+    return redirect("https://" + hostName + "yay/", code=302)
     # return send_file("./Resources/dye.mp3",as_attachment=True)
 
 @app.route('/yay/', methods=['GET'])
 def yay():
     print("## Headers in yay: ", request.headers)
-    return send_file("./Resources/dye.mp3",as_attachment=True)
+    return send_file("./Resources/dye.mp3", as_attachment=True)
 
-# if __name__ == "__main__":
-# app.run()
-serve(app, host="127.0.0.1", port=1245)
+if __name__== "__main__":
+    port = os.environ.get("port", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
+    # serve(app, host=hostName)
